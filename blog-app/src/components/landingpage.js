bb3b1b77d2013d9780934873128d72bec00c2aa7
Hunter Harris <106422074+UczenBlood@users.noreply.github.com>
2022-08-09 08:18:55 -0700

    Update landingpage.js

--- blog-app/src/components/landingpage.js
+++ blog-app/src/components/landingpage.js
@@ -3,7 +3,7 @@ import Footer from "./footer";
 
 const LandingPage =() =>{
     <>
-    <h1>Welcome to Goat Ropers</h1>
+    <h1 className="LandingH1">Welcome to Goat Ropers&#174;</h1>
     <br />
     <Footer />
     </>
