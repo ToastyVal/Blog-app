44be4be11d9cd433da22247791290441d9aa1fc2
Hunter Harris <106422074+UczenBlood@users.noreply.github.com>
2022-08-09 08:07:26 -0700

    worked on landing page

--- blog-app/src/components/footer.js
+++ blog-app/src/components/footer.js
@@ -13,7 +13,7 @@ const Footer = () => (
         </ul>
         <ul>
             {/* these are the links to things websites need */}
-            <li>Help</li>
+            <li>Help</li> 
             <li><a href="https://policies.google.com/terms?hl=en-US">Terms of Service</a></li>
             <li><a href="https://policies.google.com/privacy?hl=en-US">Privacy</a></li>
             {/* made a content policy componenet, needs to go to the content policy component */}
---
+++ blog-app/src/components/landingpage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Footer from "./footer";
+
+const LandingPage =() =>{
+    <>
+    <h1>Welcome to Goat Ropers</h1>
+    <br />
+    <Footer />
+    </>
+}
+export default LandingPage
