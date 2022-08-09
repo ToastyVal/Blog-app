86c30d5e229f685436492e188618b7775e1a0ea8
Malik DePasse <92892753+ToastyVal@users.noreply.github.com>
2022-08-09 06:50:07 -0700

    Update navBar.js

--- blog-app/src/components/navBar.js
+++ blog-app/src/components/navBar.js
@@ -1,7 +1,14 @@
-import React from "react";
-const navBar=() =>(
-    <>
-    <h2>Nav Bar</h2>
-    </>
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+
+const NavBar = () => (
+    // wrapped the whole nav in a header
+    <header>
+        <h1>Goat Ropers &#174;</h1>
+    </header>
+>>>>>>> Stashed changes
 )
 export default navBar;
