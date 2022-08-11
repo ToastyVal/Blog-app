96ff45e931d28c76bcffbed0842703d50eaae6ea
Hunter Harris <106422074+UczenBlood@users.noreply.github.com>
2022-08-11 06:49:08 -0700

    tried to make sql work

--- blog-app/src/components/comments.js
+++ blog-app/src/components/comments.js
@@ -1,12 +1,47 @@
-import React from "react";
-const Comments = () => (
+import React, { useState } from "react";
+import Axios from "axios";
+
+function AppComments() {
+  //must create a state
+  //   const [userName, setUserName] = useState("");
+  const [comment, setComment] = useState({
+    commentUserName: "",
+    commentText: "",
+  });
+
+  function commentInfo(e) {
+    console.log(e);
+    setComment({ ...comment, [e.target.name]: e.target.value });
+  }
+
+  //   const PostComment = () => {};
+  //   Axios.post("hhtp://localhost:3001/api/insert", {
+  //     userName: userName,
+  //     comment: comment,
+  //   }).then(() => {
+  //     alert("successful insert");
+  //   });
+
+  return (
     <>
-        <form id='commentForm'>
-            <label for='commentText'>Egg On</label>
-            <br />
-            <input id="commentText" type='text' placeholder="Whacha thinking partner?"></input>
-            <button>Get-er-Done</button>
-        </form>
+      <form id="commentForm">
+        <label for="commentText">Egg On</label>
+        <br />
+        <input
+          id="commentUserName"
+          type="text"
+          placeholder="User Name"
+          onChange={commentInfo}
+        />
+        <input
+          id="commentText"
+          type="text"
+          placeholder="Whacha thinking partner?"
+          onChange={commentInfo}
+        />
+        <button>Get-er-Done</button>
+      </form>
     </>
-)
-export default Comments;
+  );
+}
+export default AppComments;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,46 @@
+// import AppComments from "../../blog-app/src/components/comments.js";
+const AppComments = require("../../blog-app/src/components/comments.js");
+// import { Express } from "express";
+// import bodyParser from "body-parser";
+// import { CorsOptions } from "cors";
+// import mysql from "mysql";
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+const mysql = require("mysql");
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "$4Happytesla",
+  database: "blogdb",
 });
 
-app.listen(3001, () => {
-  console.log("running on port 3001");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//this works
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = `INSERT INTO comments (userName , textComment) VALUES ('${commentInfo.commentUserName}', '${commentInfo.commentText}');`;
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
 });
+
+// app.post("/api/insert", (req, res) => {
+//   console.log("app.post was called");
+//   const userName = req.body.userName;
+//   const comment = req.body.comment;
+//   const sqlInsert = `INSERT INTO comments (userName , textComment) VALUES (?,?);`;
+//   db.query(sqlInsert, [userName, comment], (err, result) => {
+//     console.log(result);
+//   });
+// });
+
+//run this to start the sql server
+// nodemon devStart
