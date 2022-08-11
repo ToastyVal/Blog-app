5daa2e9f56cdd06f40113e79d82054d170066c52
Hunter Harris <106422074+UczenBlood@users.noreply.github.com>
2022-08-11 07:23:36 -0700

    another failed attempt to get sql to work

--- blog-app/src/components/comments.js
+++ blog-app/src/components/comments.js
@@ -1,18 +1,42 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import Axios from "axios";
 
-function AppComments() {
+class AppComments extends Component {
   //must create a state
-  //   const [userName, setUserName] = useState("");
-  const [comment, setComment] = useState({
-    commentUserName: "",
-    commentText: "",
-  });
-
-  function commentInfo(e) {
-    console.log(e);
-    setComment({ ...comment, [e.target.name]: e.target.value });
+  constructor() {
+    super();
+    this.state = { user: {} };
+    this.onSubmit = this.handleSubmit.bind(this);
   }
+  handleSubmit(e) {
+    e.preventDefault();
+    var self = this;
+    // On submit of the form, send a POST request with the data to the server.
+    fetch("http://localhost:3000", {
+      method: "POST",
+      body: {
+        UserName: self.refs.UserName,
+        CommentInfo: self.refs.CommentInfo,
+      },
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (body) {
+        console.log(body);
+      });
+  }
+
+  //   const [userName, setUserName] = useState("");
+  //   const [comment, setComment] = useState({
+  //     commentUserName: "",
+  //     commentText: "",
+  //   });
+
+  //   function commentInfo(e) {
+  //     console.log(e);
+  //     setComment({ ...comment, [e.target.name]: e.target.value });
+  //   }
 
   //   const PostComment = () => {};
   //   Axios.post("hhtp://localhost:3001/api/insert", {
@@ -21,27 +45,28 @@ function AppComments() {
   //   }).then(() => {
   //     alert("successful insert");
   //   });
-
-  return (
-    <>
-      <form id="commentForm">
-        <label for="commentText">Egg On</label>
-        <br />
-        <input
-          id="commentUserName"
-          type="text"
-          placeholder="User Name"
-          onChange={commentInfo}
-        />
-        <input
-          id="commentText"
-          type="text"
-          placeholder="Whacha thinking partner?"
-          onChange={commentInfo}
-        />
-        <button>Get-er-Done</button>
-      </form>
-    </>
-  );
+  render() {
+    return (
+      <>
+        <form id="commentForm">
+          <label for="commentText">Egg On</label>
+          <br />
+          <input
+            id="commentUserName"
+            type="text"
+            placeholder="User Name"
+            ref="UserName"
+          />
+          <input
+            id="commentText"
+            type="text"
+            placeholder="Whacha thinking partner?"
+            ref="CommentInfo"
+          />
+          <button>Get-er-Done</button>
+        </form>
+      </>
+    );
+  }
 }
 export default AppComments;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 // import AppComments from "../../blog-app/src/components/comments.js";
-const AppComments = require("../../blog-app/src/components/comments.js");
+// const AppComments = require("../../blog-app/src/components/comments.js");
 // import { Express } from "express";
 // import bodyParser from "body-parser";
 // import { CorsOptions } from "cors";
@@ -9,6 +9,11 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const mysql = require("mysql");
+app.use(
+  express.json({
+    type: ["application/json", "text/plain"],
+  })
+);
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -17,19 +22,40 @@ const db = mysql.createConnection({
   database: "blogdb",
 });
 
-app.use(cors());
+// app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 //this works
-db.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = `INSERT INTO comments (userName , textComment) VALUES ('${commentInfo.commentUserName}', '${commentInfo.commentText}');`;
-  db.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
+// db.connect(function (err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql = `INSERT INTO comments (userName , textComment) VALUES ('${AppComments.commentUserName}', '${AppComments.commentText}');`;
+//   db.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("1 record inserted");
+//   });
+// });
+
+app.post("/", function (req, res) {
+  res.set("Access-Control-Allow-Origin", "*");
+  const { UserName, CommentInfo } = req.body;
+  let connection = new sql.ConnectionPool(db, function (err) {
+    let request = new sql.Request(connection);
+    request.query(
+      "insert into comments (userName , textComment) values ('" +
+        UserName +
+        "', '" +
+        CommentInfo +
+        "')"
+    );
+    console.log("info sent");
   });
+  res.send({ message: "Success" });
+});
+
+app.listen(3001, () => {
+  console.log("Server is running..");
 });
 
 // app.post("/api/insert", (req, res) => {
