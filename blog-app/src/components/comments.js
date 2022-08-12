7ac2872a83e5af102139f656ba49da2045ddf14b
Hunter Harris <106422074+UczenBlood@users.noreply.github.com>
2022-08-12 06:40:26 -0700

    hope this works

--- blog-app/src/components/comments.js
+++ blog-app/src/components/comments.js
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
+// <<<<<<< HEAD
 import React, { Component, useState } from "react";
 import Axios from "axios";
-=======
+// =======
 import React, { useState } from "react";
 // import Axios from "axios";
->>>>>>> e19c247d1ca9bda81281287fefdf95cfac9b6393
+// >>>>>>> e19c247d1ca9bda81281287fefdf95cfac9b6393
 
 class AppComments extends Component {
   //must create a state
