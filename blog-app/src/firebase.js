6d910a5f2a71fdcc4ac2172e5e42156e0a718009
Hunter Harris <106422074+UczenBlood@users.noreply.github.com>
2022-08-12 20:08:02 -0700

    Update firebase.js

--- blog-app/src/firebase.js
+++ blog-app/src/firebase.js
@@ -1,25 +1,25 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "@firebase/firestore";
-import { FirebaseApp } from "firebase/app";
-import "firebase/database";
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "@firebase/firestore";
+// import { FirebaseApp } from "firebase/app";
+// import "firebase/database";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB7LxQXi1Zu-4mhyqeALDEdANPNiIbwivc",
-  authDomain: "apprenticenowblog.firebaseapp.com",
-  projectId: "apprenticenowblog",
-  storageBucket: "apprenticenowblog.appspot.com",
-  messagingSenderId: "776744699243",
-  appId: "1:776744699243:web:d33a3dcbd5979b839b382e",
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB7LxQXi1Zu-4mhyqeALDEdANPNiIbwivc",
+//   authDomain: "apprenticenowblog.firebaseapp.com",
+//   projectId: "apprenticenowblog",
+//   storageBucket: "apprenticenowblog.appspot.com",
+//   messagingSenderId: "776744699243",
+//   appId: "1:776744699243:web:d33a3dcbd5979b839b382e",
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// const db = getFirestore(app);
 
-export { auth, db };
+// export { auth, db };
