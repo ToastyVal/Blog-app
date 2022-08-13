822cd5fc1c3532aefbc27120b3e341274070c51b
Hunter Harris <106422074+UczenBlood@users.noreply.github.com>
2022-08-12 20:12:25 -0700

    Start of Java server

---
+++ javaServer/src/test/java/com/GoatRopers/main/GoatRopersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GoatRopers.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoatRopersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
