ee3df913a22d7972fc1c348f2d59d91503674738
Hunter Harris <106422074+UczenBlood@users.noreply.github.com>
2022-08-15 12:11:22 -0700

    Update aboutUs.js

--- blog-app/src/components/about/aboutUs.js
+++ blog-app/src/components/about/aboutUs.js
@@ -9,14 +9,12 @@ function AboutUs() {
       <div class="cols">
         <div class="col" ontouchstart="this.classList.toggle('hover');">
           <div class="container">
-            <div
-              class="front" id="brandon"
-            >
-            </div>
+            <div class="front" id="brandon"></div>
             <div class="back">
               <div class="inner">
                 <p>
-                  I did some stuff and some things.
+                  Project Manager and team leader of these kids, I did some
+                  stuff and some things.
                 </p>
               </div>
             </div>
@@ -24,12 +22,12 @@ function AboutUs() {
         </div>
         <div class="col" ontouchstart="this.classList.toggle('hover');">
           <div class="container">
-            <div class="front" id="hunter">
-            </div>
+            <div class="front" id="hunter"></div>
             <div class="back">
               <div class="inner">
                 <p>
-                I’m Harris, Hunter Scott Harris…. That’s it. That’s all you get for my bio.
+                  I’m Harris, Hunter Scott Harris…. That’s it. That’s all you
+                  get for my bio.
                 </p>
               </div>
             </div>
@@ -37,57 +35,45 @@ function AboutUs() {
         </div>
         <div class="col" ontouchstart="this.classList.toggle('hover');">
           <div class="container">
-            <div
-              class="front" id="jude"
-            >
-              <div class="inner">
-              </div>
+            <div class="front" id="jude">
+              <div class="inner"></div>
             </div>
             <div class="back">
               <div class="inner">
-                <p>
-                  One pancake short of a full-stack.
-                </p>
+                <p>One pancake short of a full-stack.</p>
               </div>
             </div>
           </div>
         </div>
         <div class="col" ontouchstart="this.classList.toggle('hover');">
           <div class="container">
-            <div
-              class="front" id="malik"
-            >
-              <div class="inner">
-              </div>
+            <div class="front" id="malik">
+              <div class="inner"></div>
             </div>
             <div class="back">
               <div class="inner">
                 <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Alias cum repellat velit quae suscipit c.
+                  I carry in Val and is constantly called a cheater due to my
+                  crazy skills!
                 </p>
               </div>
             </div>
           </div>
         </div>
-        { <div class="col" ontouchstart="this.classList.toggle('hover');">
-          <div class="container">
-            <div
-              class="front" id="dominico"
-            >
-              <div class="inner">
+        {
+          <div class="col" ontouchstart="this.classList.toggle('hover');">
+            <div class="container">
+              <div class="front" id="dominico">
+                <div class="inner"></div>
               </div>
-            </div>
-            <div class="back">
-              <div class="inner">
-                <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Alias cum repellat velit quae suscipit c.
-                </p>
+              <div class="back">
+                <div class="inner">
+                  <p>Full stack dev with the heart of a lion!</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>}
+        }
       </div>
     </div>
   );
