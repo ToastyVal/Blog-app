f93b3ee11efa0a1bf3fc47d69a0d3a646a613b20
BrandonBakerCS <109840185+BrandonBakerCS@users.noreply.github.com>
2022-08-15 07:42:13 -0700

    Changed something

---
+++ main/src/main/java/com/goatropers/main/service/LoginServiceImpl.java
@@ -0,0 +1,29 @@
+package com.goatropers.main.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.goatropers.main.model.Login;
+import com.goatropers.main.repository.LoginRepository;
+
+@Service
+public class LoginServiceImpl implements LoginService {
+
+    @Autowired
+    public LoginRepository loginRepository;
+
+    @Override
+    public Login saveLogin(Login login) {
+        // TODO Auto-generated method stub
+        return loginRepository.save(login);
+    }
+
+    @Override
+    public List<Login> getAllLogins() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
